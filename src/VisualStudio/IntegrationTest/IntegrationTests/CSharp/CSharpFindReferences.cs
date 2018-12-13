1b8761541843496705274bcfea01313f3dfc1825
Gen Lu <gel@microsoft.com>
2018-12-13 11:37:37 -0800

    Revert "close sqlite connection when solution is closed (#31687)"
    This reverts commit 4cc40d05fe601347f8efa7fc75b8626463a54b49.

--- src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpFindReferences.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpFindReferences.cs
@@ -158,11 +158,6 @@ class Program
 
             // verify working folder has set
             Assert.NotNull(VisualStudio.Workspace.GetWorkingFolder());
-
-            VisualStudio.SolutionExplorer.CloseSolution();
-
-            // verify working folder has not set
-            Assert.Null(VisualStudio.Workspace.GetWorkingFolder());
         }
     }
 }
